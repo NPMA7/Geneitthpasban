de4dadb987ac14cdb73bd045b9eeb2a31fddb1b8
Pasha Malik <pashamalik9371@gmail.com>
2024-08-02 11:22:09 +0700

    test 2

--- public/js/script.js
+++
@@ -1,5 +0,0 @@
-// PAGE RENDERED /SECONDS
-window.addEventListener("load", () => {
-  const renderTime = (performance.now() / 1000).toFixed(4); // Menghitung waktu render dalam detik
-  document.getElementById("renderTimeValue").textContent = renderTime;
-});
